34f8d4d73b4dc6b8e0d61ab05998600486aca03a
Ragulram B <ragulram0427@gmail.com>
2025-08-31 12:26:21 +0530

    Update Layout.jsx

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -6,6 +6,36 @@ import Sidebar from './Sidebar';
 const Layout = () => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
 
+  // return (
+  //   <div className="flex h-screen w-full overflow-hidden">
+  //     {/* Sidebar */} 
+  //     <div
+  //       className={`fixed inset-y-0 left-0 z-20 h-full transform transition-transform duration-300 ease-in-out 
+  //       ${sidebarVisible ? "translate-x-0" : "-translate-x-full"} 
+  //       w-54`}
+  //     >
+  //       <Sidebar
+  //         sidebarVisible={sidebarVisible}
+  //         setSidebarVisible={setSidebarVisible}
+  //       />
+  //     </div>
+
+  //     {/* Main Content */}
+  //     <div
+  //       className={`flex-1 p-2 lg:p-4 bg-background overflow-y-auto transition-all duration-300 ease-in-out 
+  //       ${sidebarVisible ? "lg:ml-54" : "lg:ml-0"}`}
+  //     >
+  //       <Navbar
+  //         sidebarVisible={sidebarVisible}
+  //         setSidebarVisible={setSidebarVisible}
+  //       />
+  //       <div className="mt-4">
+  //         <Outlet />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
   return (
     <div className="flex h-screen w-full overflow-hidden">
       {/* Sidebar */}
